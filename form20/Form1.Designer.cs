9fcbe0b968f30d17c462a92353314359207d30b7
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-09 12:05:25 +0300

    Add files via upload

---
+++ form20/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace form20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtYaz = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtYaz
+            // 
+            this.txtYaz.Location = new System.Drawing.Point(143, 72);
+            this.txtYaz.Name = "txtYaz";
+            this.txtYaz.Size = new System.Drawing.Size(100, 20);
+            this.txtYaz.TabIndex = 0;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(143, 128);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 33);
+            this.btnHesapla.TabIndex = 1;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 299);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtYaz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtYaz;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
